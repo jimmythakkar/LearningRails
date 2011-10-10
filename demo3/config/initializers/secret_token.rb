b471081714434d75770d79ae6f5e254333d01448
Jimmy <jimmythakkar007@gmail.com>
2011-10-10 11:03:07 +0530

    Updating a new demo app which has models along with views and controllers

---
+++ demo3/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo3::Application.config.secret_token = '4731100b04651fce4b9fd31e6ee29442ef00c056cc6f3536a2561160622a8b020b75d5e25e2ff096b2c2d69750ec3b8cd6271e6645bafd6cfc4c4edd9a3992c2'
